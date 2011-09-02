111a40cf09886139adb5722f5b567345fe5dd686
Christopher Wu <chriswu222@gmail.com>
2011-09-02 03:51:10 -0400

    Functional tests

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,46 +1,57 @@
-test "product price must be positive" do
-  product = Product.new(:title       => "My Book Title",
-                        :description => "yyy",
-                        :image_url   => "zzz.jpg")
-  product.price = -1
-  assert product.invalid?
-  assert_equal "must be greater than or equal to 0.01",
-    product.errors[:price].join('; ')
+require 'test_helper'
 
-  product.price = 0
-  assert product.invalid?
-  assert_equal "must be greater than or equal to 0.01",
-    product.errors[:price].join('; ')
+class ProductTest < ActiveSupport::TestCase 
+  fixtures :products
 
-  product.price = 1
-  assert product.valid?
-end
+  test "product price must be positive" do
 
-def new_product(image_url)
-  Product.new(:title       => "My Book Title",
-              :description => "yyy",
-              :price       => 1,
-              :image_url   => image_url)
-end
+    product = Product.new(:title       => "My Book Title",
+                          :description => "yyy",
+                          :image_url   => "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
 
-test "image url" do
-  ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(:title       => "My Book Title",
+                :description => "yyy",
+                :price       => 1,
+                :image_url   => image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
            http://a.b.c./x/y/z/fred.gif }
-        
-  bad = %w{ fred.doc fred.gif/more fred.give.more }
 
-  ok.each do |name|
-    assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    bad = %w{ fred.doc fred.gif/more fred.give.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} should be invalid"
+    end
   end
 
-  bad.each do |name|
-    assert new_product(name).invalid?, "#{name} should be invalid"
+  test "product is not valid without a unique title" do
+    product = Product.new(:title       => products(  :ruby).title,
+                          :description => "yyy",
+                          :price       => 1,
+                          :image_url   => "fred.gif")
+
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
   end
+
 end
-
-
-
-
-
-
-
